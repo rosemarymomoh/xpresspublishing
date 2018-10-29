d0b3882d7f02e0a83ae01312e8615a6f801ee254
Momoh <oziohulove1@gmail.com>
2018-10-29 17:56:22 -0500

    server up and running

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const errorhandler = require('errorhandler');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//bodyParsing Middleware
+app.use(bodyParser.json());
+
+// Logging Middleware
+app.use(morgan('dev'));
+
+
+//Development error-handling/debugging.
+app.use(errorhandler());
+
+app.use(cors());
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
+
+module.exports = app;
